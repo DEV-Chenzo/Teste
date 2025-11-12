c9fb1d9f777c51c317a37efb1a0682b3348f2a56
Tiagotj7 <tiagowork07@gmail.com>
2025-11-12 17:32:10 -0300

    edit cred db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'sql208.infinityfree.com';
-$dbname = 'if0_40402164';
-$username = 'hnoVA64NVi7f';
-$password = 'crud_project';
+$dbname = 'crud_project';
+$username = 'if0_40402164';
+$password = 'hnoVA64NVi7f';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
