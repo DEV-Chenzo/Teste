b802a9ac8ade6c6313c2f07880dbd50f440b7aea
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2025-11-12 16:51:39 -0300

    error

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /* config.php – informações de conexão ao MySQL */
 
-$host     = ' sql208.infinityfree.com ';      // ou IP do servidor de BD
+$host     = 'sql208.infinityfree.com';      // ou IP do servidor de BD
 $user     = 'if0_40402164';           // usuário do MySQL
 $password = 'hnoVA64NVi7f';               // senha do usuário
 $dbName   = 'crud_project';      // nome da base criada acima
